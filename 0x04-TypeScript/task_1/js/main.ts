22d16cffa923dff0cd34f3caa88f34822096dfff
Matilda Esenam Gbeve <esenam16@gmail.com>
2025-09-13 14:30:42 +0100

    Directors interface extending Teacher

--- 0x04-TypeScript/task_1/js/main.ts
+++ 0x04-TypeScript/task_1/js/main.ts
@@ -18,3 +18,19 @@ const teacher3: Teacher = {
 };
 
 console.log(teacher3);
+
+// Directors extends Teacher
+interface Directors extends Teacher {
+  numberOfReports: number;
+}
+
+// Example director
+const director1: Directors = {
+  firstName: "John",
+  lastName: "Doe",
+  location: "London",
+  fullTimeEmployee: true,
+  numberOfReports: 17,
+};
+
+console.log(director1);
